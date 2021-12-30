ec6375368d34fddabdeb57af9088423f0707b3f2
Mohamed Emir Bouhamar <87539839+medemirbhm1@users.noreply.github.com>
2021-12-30 13:58:15 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,3 +20,15 @@ cards.forEach(card => card.addEventListener("mouseover", ()=> {
     cards.forEach(card => card.classList.remove("active"))
     card.classList.add("active")
 }))
+
+// ==== SCROLL REVEAL ANIMATION  =====
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2000,
+  delay: 300,
+});
+sr.reveal(".home .image, .services .container, footer ul ");
+sr.reveal(".home  .text", { delay: 400 });
+sr.reveal(".order .image, .deliver .text, .track .image, .app .image", { origin: "left" });
+sr.reveal(".order .text, .deliver .image, .track .text, .app .text", { origin: "right" });
